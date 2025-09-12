10aaced6f7c30ea929dd339e963acbb51cda6339
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2025-09-11 22:40:17 -0300

    feat:Created basic frontend form to login or sign up

---
+++ Authentication.frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "./Components/Loginform";
+
+export default function App() {
+  return (
+    <LoginForm/>
+  )
+}
+
---
+++ Authentication.frontend/src/Components/SignIn.tsx
@@ -0,0 +1,81 @@
+import { FaGoogle, FaLinkedinIn, FaFacebookF } from "react-icons/fa";
+
+import React from "react";
+function SignInForm() {
+    const [state, setState] = React.useState({
+        email: "",
+        password: ""
+    });
+    const handleChange = (evt: { target: { value: any; name: any; }; }) => {
+        const value = evt.target.value;
+        setState({
+            ...state,
+            [evt.target.name]: value
+        });
+    };
+
+    const handleOnSubmit = (evt: { preventDefault: () => void; }) => {
+        evt.preventDefault();
+
+        const { email, password } = state;
+
+        if(email === "teste@gmail.com" && password === "1234") {
+           alert(`You are login with email: ${email} and password: ${password}`);
+            return;
+        }
+        else{
+            alert("Wrong email or password");
+        }
+
+        for (const key in state) {
+            setState({
+                ...state,
+                [key]: ""
+            });
+        }
+    };
+
+    return (
+        <div className="form-container sign-in-container">
+            <form onSubmit={handleOnSubmit}>
+                <h1>Sign in</h1>
+                <div className="social-container">
+                    <a href="#" className="social">
+                        <i className="fab fa-facebook-f" >
+                            <FaFacebookF />
+                        </i>
+                    </a>
+                    <a href="#" className="social">
+                        <i className="fab fa-google-plus-g" >
+                            <FaGoogle />
+                        </i>
+                    </a>
+                    <a href="#" className="social">
+                        <i className="fab fa-linkedin-in" >
+                            <FaLinkedinIn />
+                        </i>
+                    </a>
+                </div>
+                <span>or use your account</span>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    value={state.email}
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={state.password}
+                    onChange={handleChange}
+                />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignInForm;
